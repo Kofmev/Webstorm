4aaa04e8972faf8b17f51fb73650323869ba87f1
Kevin <kevin.verbeek@flitsmeister.nl>
2019-05-29 16:03:48 +0200

    working submit & post

--- javascript/FormValidation.js
+++ javascript/FormValidation.js
@@ -1,7 +1,7 @@
 function validateForm() {
-    const x = document.forms["myForm"]["firstname"].value;
-    const y = document.forms["myForm"]["lastname"].value;
-    const z = document.forms["myForm"]["email"].value;
+    const x = document.forms["form"]["firstname"].value;
+    const y = document.forms["form"]["lastname"].value;
+    const z = document.forms["form"]["email"].value;
     if (x === "") {
         alert("Je moet je voornaam nog invullen");
         return false;
@@ -16,10 +16,17 @@ function validateForm() {
     }
 }
 
-function writeName(){
+function writeName() {
 
-    var welcome = document.getElementById('welcome ');
-    var name = document.getElementById('name');
+    var welcome = document.getElementById('welcome');
+    var firstName = document.getElementById('firstname');
+    var lastName = document.getElementById('lastname');
+    var email = document.getElementById('email');
+    var formContent = document.getElementById('form');
 
-    welcome.innerHTML = "welcome" + name.value+".<br/>"
+
+    welcome.innerHTML = "Hey " + firstName.value + " " + lastName.value + ".<br>"
+        + email.value + "<br>";
+
+    formContent.innerHTML = "";
 }
